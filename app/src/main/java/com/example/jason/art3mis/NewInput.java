1ee209ab4de3db60f0ad01f5df12047a009a3ba6
Jason Tang <1999jasontang@gmail.com>
2018-07-22 18:33:23 -0700

    A bit of house cleaning

--- app/src/main/java/com/example/jason/art3mis/NewInput.java
+++ app/src/main/java/com/example/jason/art3mis/NewInput.java
@@ -2,7 +2,6 @@ package com.example.jason.art3mis;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -14,39 +13,33 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class NewInput extends AppCompatActivity {
 	
 	int totalHeight;
 	int totalWidth;
+	ScrollViewWithMaxHeight sv_Max;
+	EditText et_course_name;
+	Button b_work;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_input);
 		
-		
-		ScrollViewWithMaxHeight sc = findViewById(R.id.scroll_view_max_height);
-		EditText et_course_name = findViewById(R.id.course_name);
-		Button but = findViewById(R.id.add_work_button);
+		sv_Max = findViewById(R.id.scroll_view_max_height);
+		et_course_name = findViewById(R.id.course_name);
+		b_work = findViewById(R.id.add_work_button);
 		
 		int etHeight = et_course_name.getHeight();
-		int butHeight = but.getHeight();
+		int b_work_height = b_work.getHeight();
 		totalHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
 		totalWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
 		
-		sc.setMaxHeight(totalHeight - etHeight - butHeight);
+		sv_Max.setMaxHeight(totalHeight - etHeight - b_work_height);
 		
 	}
 	
@@ -152,94 +145,6 @@ public class NewInput extends AppCompatActivity {
 			courseName + "\n" + strAssignNames + "\n" + strAssignWorths
 		);
 		
-		
-		
-		
-		// Saving to Storage
-//		try {
-//			File file = new File(courseName +".csv");
-//			if (!file.exists()) {
-//				file.createNewFile();
-//			}
-//			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-//			BufferedWriter bw = new BufferedWriter(fw);
-//			bw.write(strAssignNames);
-//			bw.write(strAssignWorths);
-//			bw.close();
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//			display.setText("BEEP BOOP SOMETHING IS WRONG");
-//		}
-		try{
-			File csvfile = new File(Environment.getExternalStorageDirectory() + "/csvfile.csv");
-			CSVReader reader = new CSVReader(new FileReader("csvfile.getAbsolutePath()"));
-			String [] nextLine;
-			while ((nextLine = reader.readNext()) != null) {
-				// nextLine[] is an array of values from the line
-				System.out.println(nextLine[0] + nextLine[1] + "etc...");
-			}
-		} catch(Exception e){
-			e.printStackTrace();
-			Toast.makeText(this, "The specified file was not found", Toast.LENGTH_SHORT).show();
-		}
-		
-//		try {
-//			String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-//			String fileName = courseName + ".csv";
-//			String filePath = baseDir + File.separator + fileName;
-//			File f = new File(filePath );
-//			CSVWriter writer;
-//			// File exist
-//			if(f.exists() && !f.isDirectory()){
-//				FileWriter mFileWriter = new FileWriter(filePath , true);
-//				writer = new CSVWriter(mFileWriter);
-//			}
-//			else {
-//				writer = new CSVWriter(new FileWriter(filePath));
-//			}
-//
-//			writer.writeNext(assignNames);
-////			writer.writeNext(assignWorths);
-//
-//			writer.close();
-//		} catch (IOException e) {
-//			display.setText("BEEP BOOP SOMETHING IS WRONG");
-//		}
-		
-	}
-
-	public void saveWork2(View v) {
-		
-		try {
-			String baseDir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-			String fileName = "memes.csv";
-			String filePath = baseDir + File.separator + fileName;
-			File f = new File(filePath );
-			CSVWriter writer;
-			
-			if(f.exists() && !f.isDirectory()){
-				Toast.makeText(this, filePath, Toast.LENGTH_SHORT).show();
-				FileWriter mFileWriter = new FileWriter(filePath , true);
-				writer = new CSVWriter(mFileWriter);
-			}
-			else {
-				FileWriter mFileWriter = new FileWriter(filePath , true);
-//				writer = new CSVWriter();
-			}
-//
-//			}
-//			else {
-//				writer = new CSVWriter(new FileWriter(filePath));
-//			}
-//
-//			String[] entries = "first#second#third".split("#");
-//			writer.writeNext(entries);
-//			writer.close();
-			
-		} catch (Exception e) {
-			Toast.makeText(this, "Beep Boop Big Error", Toast.LENGTH_SHORT).show();
-		}
 	}
 	
 	public void write(View v) {
@@ -251,7 +156,7 @@ public class NewInput extends AppCompatActivity {
 		try {
 			FileOutputStream fos = openFileOutput("memes.txt", Context.MODE_PRIVATE);
 			fos.write(str.getBytes());
-			fos.write("#".getBytes());
+			fos.write(System.getProperty("line.separator").getBytes());
 			fos.write(str2.getBytes());
 			fos.close();
 			Toast.makeText(this, "Save Success", Toast.LENGTH_SHORT).show();
@@ -276,7 +181,10 @@ public class NewInput extends AppCompatActivity {
 	}
 	
 	public void fancyRead(View v) {
-	
+		// Seperate
+		// Take the read string, cut off the brackets
+		// Regex split by ", " and then make a for loop, casting them as ints
+		// then add
 	}
 	
 }
